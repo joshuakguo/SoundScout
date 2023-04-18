5472d6c70d7b1c28486a7909a3d9feb0660fb29c
Joshua G <129023025+belowcolorful@users.noreply.github.com>
2023-04-17 21:55:18 -0400

    changes to two ports

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,29 @@
-import React, { MouseEventHandler } from 'react';
-import './App.css';
+import React, { MouseEventHandler, useEffect } from "react"
+import "./App.css"
 
 function App() {
+  useEffect(() => {
+    fetch("http://localhost:5000/start")
+  }, [])
+
   return (
     <div className="full-body-container">
-    <div className="parallax-wrap">
-      <div className="background-grid"></div>
-    </div>
-    <div className="top-text">
-      <div className="title">
-        <h1>SoundScout</h1>
+      <div className="parallax-wrap">
+        <div className="background-grid"></div>
       </div>
-      <div className="input-box" onClick={sendFocus}>
-        <img src="mag.png" onClick={search} />
-        <input placeholder="Search for a playlist" id="filter-text-val" />
+      <div className="top-text">
+        <div className="title">
+          <h1>SoundScout</h1>
+        </div>
+        <div className="input-box" onClick={sendFocus}>
+          <img src="mag.png" onClick={search} />
+          <input placeholder="Search for a playlist" id="filter-text-val" />
+        </div>
       </div>
+      <div id="answer-box"></div>
+      <footer>Created by Harrison Chin, Willy Jiang, Joshua Guo, Eric Huang, Alex Levinson</footer>
     </div>
-    <div id="answer-box"></div>
-    <footer>Created by Harrison Chin, Willy Jiang, Joshua Guo, Eric Huang, Alex Levinson</footer>
-  </div>
-  );
+  )
 }
 
 function answerBoxTemplate(title: string, titleDesc: string) {
@@ -27,12 +31,12 @@ function answerBoxTemplate(title: string, titleDesc: string) {
 }
 
 const sendFocus: MouseEventHandler<HTMLDivElement> = (e) => {
-  (document.getElementById("filter-text-val") as HTMLInputElement).focus()
+  ;(document.getElementById("filter-text-val") as HTMLInputElement).focus()
   // this.getElementById("filter-text-val").focus()
 }
 
 const search: MouseEventHandler<HTMLImageElement> = (e) => {
-  (document.getElementById("answer-box") as HTMLDivElement).innerHTML = ""
+  ;(document.getElementById("answer-box") as HTMLDivElement).innerHTML = ""
   // console.log(document.getElementById("filter-text-val").value)
   const box: HTMLElement = document.getElementById("answer-box") as HTMLDivElement
   while (box.lastElementChild) {
@@ -40,26 +44,30 @@ const search: MouseEventHandler<HTMLImageElement> = (e) => {
   }
   fetch(
     "http://localhost:5000/search?" +
-      new URLSearchParams({ title: (document.getElementById("filter-text-val") as HTMLInputElement).value }).toString()
+      new URLSearchParams({
+        title: (document.getElementById("filter-text-val") as HTMLInputElement).value,
+      }).toString()
   )
     .then((response) => response.json())
     .then((data) =>
       data.forEach((row: string[]) => {
         let tempDiv = document.createElement("div")
         tempDiv.className = "answer"
-        tempDiv.innerHTML = answerBoxTemplate(row[0], row[3]);
-        (document.getElementById("answer-box") as HTMLDivElement).appendChild(tempDiv)
+        tempDiv.innerHTML = answerBoxTemplate(row[0], row[3])
+        ;(document.getElementById("answer-box") as HTMLDivElement).appendChild(tempDiv)
       })
     )
 }
 
 const parallax = (event: MouseEvent) => {
-  const x = (window.innerWidth - event.pageX * 1) / 90;
-  const y = (window.innerHeight - event.pageY * 1) / 90;
+  const x = (window.innerWidth - event.pageX * 1) / 90
+  const y = (window.innerHeight - event.pageY * 1) / 90
 
-  (document.querySelector(".parallax-wrap .background-grid") as HTMLDivElement).style.transform = `translateX(${x}px) translateY(${y}px)`
+  ;(
+    document.querySelector(".parallax-wrap .background-grid") as HTMLDivElement
+  ).style.transform = `translateX(${x}px) translateY(${y}px)`
 }
 
 document.addEventListener("mousemove", parallax)
 
-export default App;
+export default App
